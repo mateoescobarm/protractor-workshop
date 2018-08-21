827f0d138379dcf7577b2fee7d55c2b53bd09353
mateoescobarm <mateoescobarm@gmail.com>
2018-08-21 00:24:29 -0500

    deactivated promise manager and selenium server

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,11 +1,13 @@
 import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  SELENIUM_PROMISE_MANAGER: false,
   specs: ['../test/Google.spec.js'],
   noGlobals: true,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    reporter();
   }
 }
