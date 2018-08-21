181bacb4f70da380f6eaa3ab012edf1bf356e71c
mateoescobarm <mateoescobarm@gmail.com>
2018-08-21 00:24:29 -0500

    adding console report, styl trowing error

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,9 +3,12 @@ import { browser, Config } from 'protractor';
 export const config: Config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['../test/Google.spec.js'],
+  specs: ['../test/Google.spec.ts'],
   noGlobals: true,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
   }
 }
+
+import { reporter } from './helpers/reporter';
+reporter();
