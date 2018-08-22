fcface8f3a62b3fafc7fc76e63c8f05de40132d8
mateoescobarm <mateoescobarm@gmail.com>
2018-08-21 22:29:55 -0500

    ts-js

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,7 +3,7 @@ import { browser, Config } from 'protractor';
 export const config: Config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['../test/Google.spec.ts'],
+  specs: ['../test/Google.spec.js'],
   noGlobals: true,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
