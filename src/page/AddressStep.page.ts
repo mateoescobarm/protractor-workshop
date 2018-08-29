a8702fc24e6a718cbcdfe50d9adfeb07f2497e2c
mateoescobarm <mateoescobarm@gmail.com>
2018-08-28 21:03:08 -0500

    element to $

--- src/page/AddressStep.page.ts
+++ src/page/AddressStep.page.ts
@@ -1,8 +1,8 @@
-import { ElementFinder, promise, element, by } from 'protractor';
+import { $, ElementFinder, promise } from 'protractor';
 
 export class AdressStepPage {
   private get goToShippingButton(): ElementFinder {
-    return element(by.css('#center_column > form > p > button > span'));
+    return $('#center_column > form > p > button > span');
   }
 
   public checkAdressProceedToShipping(): promise.Promise<void> {
