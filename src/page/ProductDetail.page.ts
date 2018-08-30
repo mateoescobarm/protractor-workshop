75c3327618f091f945c69ee280ccb4e4016a726f
Rene Marin <rene@su.local>
2018-08-30 16:54:38 -0500

    unused function deleted

--- src/page/ProductDetail.page.ts
+++ src/page/ProductDetail.page.ts
@@ -4,9 +4,6 @@ export class ProductDetail {
   private get addToCartButton(): ElementFinder {
     return $('#add_to_cart > button > span');
   }
-  public isOpen() {
-    return browser.wait(this.addToCartButton.isPresent, 10000)
-  }
   public addToCart(): promise.Promise<void> {
     return this.addToCartButton.click();
   }
