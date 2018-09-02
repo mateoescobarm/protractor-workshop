2d447049204161b13950364f7ee266013cabe995
mateoescobarm <40477937+mateoescobarm@users.noreply.github.com>
2018-09-02 02:05:17 -0500

    separate describes (#17)

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -12,34 +12,66 @@ import { MenuContentPage,
          ProductAdded,
          TermsOfServiceCheckBox} from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productList: ProductList = new ProductList();
-  const productDetail: ProductDetail = new ProductDetail();
-  const productAdded: ProductAdded = new ProductAdded();
-  const proceedToCheckout: SummaryStepPage = new SummaryStepPage();
-  const signIn: SignInStepPage = new SignInStepPage();
-  const adressProceed: AdressStepPage = new AdressStepPage();
-  const checkBox: TermsOfServiceCheckBox = new TermsOfServiceCheckBox();
-  const shippingStep: ShippingStepPage = new ShippingStepPage();
-  const paymentStep: PaymentStepPage = new PaymentStepPage();
-  const bankPayment: BankPaymentPage = new BankPaymentPage();
-  const orderResume: OrderResumePage = new OrderResumePage();
 
- it('then should be bought a t-shirt', async () => {
+describe('open browser', () =>{
+
+  beforeEach(async () => {
     await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productList.selecProduct();
-    await productDetail.addToCart();
-    await productAdded.proceedToCheckout();
-    await proceedToCheckout.summaryProceedToCheckout();
-    await signIn.fillForm();
-    await adressProceed.checkAdressProceedToShipping();
-    await checkBox.checkTermsOfService();
-    await shippingStep.proceedToPayment();
-    await paymentStep.selectPaymentMetodBankWire();
-    await bankPayment.goToConfirmationPage();
-    await expect(orderResume.getTextOfConfirmation())
-      .toBe('Your order on My Store is complete.');
- });
+  });
+
+  describe('buying a T-Shirt', () =>{
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productList: ProductList = new ProductList();
+    const productDetail: ProductDetail = new ProductDetail();
+    const productAdded: ProductAdded = new ProductAdded();
+    const proceedToCheckout: SummaryStepPage = new SummaryStepPage();
+
+    beforeEach(async () => {
+      await menuContentPage.goToTShirtMenu();
+      await productList.selecProduct();
+      await productDetail.addToCart();
+      await productAdded.proceedToCheckout();
+      await proceedToCheckout.summaryProceedToCheckout();
+    });
+
+    describe('sign in step', () =>{
+      const signIn: SignInStepPage = new SignInStepPage();
+
+      beforeEach(async () => {
+        await signIn.fillForm();
+      });
+
+      describe('adress and shipping steps', () =>{
+        const adressProceed: AdressStepPage = new AdressStepPage();
+        const checkBox: TermsOfServiceCheckBox = new TermsOfServiceCheckBox();
+        const shippingStep: ShippingStepPage = new ShippingStepPage();
+
+        beforeEach(async () => {
+          await adressProceed.checkAdressProceedToShipping();
+          await checkBox.checkTermsOfService();
+          await shippingStep.proceedToPayment();
+        });
+
+        describe('bankment payment', () =>{
+          const paymentStep: PaymentStepPage = new PaymentStepPage();
+          const bankPayment: BankPaymentPage = new BankPaymentPage();
+
+          beforeEach(async () => {
+            await paymentStep.selectPaymentMetodBankWire();
+            await bankPayment.goToConfirmationPage();
+          });
+        
+
+          describe('confirmation step', () => {  
+            const orderResume: OrderResumePage = new OrderResumePage();
+
+            it('then should be bought a t-shirt', async () => {
+              await expect(orderResume.getTextOfConfirmation())
+                .toBe('Your order on My Store is complete.');
+            });
+          });
+        });
+      });
+    });
+  });
 });
