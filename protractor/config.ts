cb3707ebcb965dabab8f604de6f39b71a97083f5
Rene Marin <rene@su.local>
2018-09-03 14:19:02 -0500

    capabilities for chrome

--- protractor/config.ts
+++ protractor/config.ts
@@ -10,6 +10,13 @@ export const config: Config = {
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
   },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   onPrepare: () => {
     browser.manage().timeouts().implicitlyWait(0);
     browser.ignoreSynchronization = true;
