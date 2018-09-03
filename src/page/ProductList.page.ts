8bd04143fd0d0aa1c17e4f3284ffc7771b40d712
Rene Marin <rene@su.local>
2018-09-03 15:58:26 -0500

    product added

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -1,11 +1,19 @@
-import { ElementFinder, promise, element, by } from 'protractor';
+import { ElementFinder, promise, $$, ElementArrayFinder } from 'protractor';
 
 export class ProductList {
-  private get productList(): ElementFinder {
-    return element(by.className('product_img_link'));
+  private get productContainerList(): ElementArrayFinder {
+    return $$('.product_list > li');
+  }
+
+  private findByProduct(name: string): ElementFinder {
+    return this.productContainerList.filter((product) => {
+      return product.$('a.product_img_link').getAttribute('title').then((attribute) => {
+        return attribute === name;
+      });
+    }).first();
   }
 
   public selecProduct(): promise.Promise<void> {
-    return this.productList.click();
+    return this.findByProduct(name).$('img').click();
   }
 }
