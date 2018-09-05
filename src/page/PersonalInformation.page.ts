218c5efea0ba147e450bd1aaf08aa754b100e656
Rene Marin <rene@su.local>
2018-09-05 17:45:34 -0500

    personal info fill form test

--- src/page/PersonalInformation.page.ts
+++ src/page/PersonalInformation.page.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import {
   ElementFinder,
@@ -90,6 +91,40 @@ export class FillPersonalInformationPage {
   }
 
   private alejandro = {
+=======
+import { ElementFinder, promise, $ } from 'protractor';
+
+export class FillPersonalInformationPage {
+  private get firstNameField(): ElementFinder {
+    return $('fisrt name');
+  }
+  private get lastNameField(): ElementFinder {
+    return $('last name');
+  }
+  private get sexCheckBox(): ElementFinder {
+    return $('sex-0');
+  }
+  private get experienceCheckBox(): ElementFinder {
+    return $('exp-6');
+  }
+  private get professionCheckBox(): ElementFinder {
+    return $('profession-1');
+  }
+  private get toolsCheckBox(): ElementFinder {
+    return $('tool-6');
+  }
+  private get continentSelector(): ElementFinder {
+    return $('#continents');
+  }
+  private get commandsSelector(): ElementFinder {
+    return $('#selenium_comands');
+  }
+  private get submitButton(): ElementFinder {
+    return $('#submit');
+  }
+
+  alejandro = {
+>>>>>>> personal info fill form test
     firstName: 'Alejandro',
     lastName: 'Perdomo',
     sex: 'Male',
@@ -103,6 +138,7 @@ export class FillPersonalInformationPage {
       'Switch Commands',
       'Wait Commands',
       'WebElement Commands']
+<<<<<<< HEAD
   };
 
   private async logInForm(personInfo) {
@@ -120,3 +156,22 @@ export class FillPersonalInformationPage {
     return this.logInForm(this.alejandro);
   }
 }
+=======
+  }
+
+  private async logInForm(personalInfo) {
+    await this.firstNameField.sendKeys(personalInfo.firstName);
+    await this.lastNameField.sendKeys(personalInfo.lastName);
+    await this.sexCheckBox.sendKeys(personalInfo.sex);
+    await this.experienceCheckBox.sendKeys(personalInfo.experience);
+    await this.professionCheckBox.sendKeys(personalInfo.profession);
+    await this.toolsCheckBox.sendKeys(personalInfo.tools);
+    await this.continentSelector.sendKeys(personalInfo.continent);
+    await this.commandsSelector.sendKeys(personalInfo.commands);
+    return this.submitButton.click();
+  }
+  public fillForm(): promise.Promise<void> {
+    return this.logInForm(this.alejandro);
+  }
+};
+>>>>>>> personal info fill form test
