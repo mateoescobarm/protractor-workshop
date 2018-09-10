df6e1eb98e4e0729916381a89d4b9c8592466470
mateoescobarm <mateoescobarm>
2018-09-09 21:59:05 -0500

    iframe WIP

--- src/page/Iframe.page.ts
+++ src/page/Iframe.page.ts
@@ -1,21 +1,23 @@
-import { ElementFinder, $, promise } from 'protractor';
+import { ElementFinder, $, promise, browser } from 'protractor';
 
 export class IFramePage {
   private get pageName (): ElementFinder {
     return $('.page-title h1');
   }
 
-  public goToIFrameOne (): promise.Promise<void> {
-    return $('#IF1').click();
-  }
-
-  public returnToGlobal (): promise.Promise<void> {
-    return $('h1').click();
-  }
-
   public globalPageName (): promise.Promise<string> {
     return this.pageName.getText();
   }
 
+  public goToIFrameOne (): promise.Promise<void> {
+    return browser.switchTo().frame($('#IF1'));
+  }
 
+  public getIFrameTitle(): promise.Promise<string> {
+    return $('h1').getText()
+  }
+
+  public returnToGlobalpage (): promise.Promise<void> {
+    return $('h1').click();
+  }
 }
