3f387e33bd53ed0e83e2a9c5cecbf8036c33eaa8
Salomon Orrego Martinez <sorregom@psl.com.co>
2018-09-14 07:26:34 -0500

    fixing long string in Locators.spec.ts

--- test/Locators.spec.ts
+++ test/Locators.spec.ts
@@ -17,7 +17,11 @@ describe('open browser', () => {
     describe('confirmation step', () => {
       it('Form Correctly Filled', async () => {
         await expect(browser.getCurrentUrl())
-          .toBe(`http://toolsqa.com/automation-practice-form/?firstname=Alejandro&lastname=Perdomo&sex=Male&exp=7&profession=Automation+Tester&photo=&tool=Selenium+Webdriver&continents=South+America&submit=`);
+          .toBe(
+            `http://toolsqa.com/automation-practice-form/?firstname=Alejandro&lastname=Perdomo\
+            &sex=Male&exp=7&profession=Automation+Tester&photo=&tool=Selenium+Webdriver\
+            &continents=South+America&submit=`
+          );
       });
     });
   });
