76dacd573ff655dccb2552173c9e949d40bd37e8
mateoescobarm <40477937+mateoescobarm@users.noreply.github.com>
2018-09-14 12:45:19 -0500

    Js iframe (#22)
    * separate describes
    
    * llll
    
    * hjcg
    
    * personal info fill form test
    
    * info test update
    
    * end line
    
    * endline 2
    
    * fixed stuff
    
    * llll
    
    * hjcg
    
    * personal info fill form test
    
    * info test update
    
    * end line
    
    * endline 2
    
    * fixed stuff
    
    * array finder update
    
    * arrs
    
    * exp ok fixin arrays
    
    * array´s messing with me
    
    * la ultima y nos vamos
    
    * checking global page name
    
    * better locator
    
    * iframe WIP
    
    * ifff
    
    * personal info fill form test
    
    * info test update
    
    * end line
    
    * endline 2
    
    * fixed stuff
    
    * hjcg
    
    * personal info fill form test
    
    * info test update
    
    * end line
    
    * endline 2
    
    * fixed stuff
    
    * array finder update
    
    * arrs
    
    * exp ok fixin arrays
    
    * array´s messing with me
    
    * la ultima y nos vamos
    
    * checking global page name
    
    * better locator
    
    * iframe WIP
    
    * ifff
    
    * titles ok, changing height WIP
    
    * working with iframes OK
    
    * OK
    
    * pi
    
    * renames
    
    * renames 2
    
    * damn
    
    * :) made it
    
    * fkn travis
    
    * fkn trvis 2
    
    * sleep added
    
    * :)
    
    * fixes
    
    * lint fixes
    
    * tslint fixes
    
    * width
    
    * width 2
    
    * width 3
    
    * meh
    
    * ...
    
    * mi locator + expected condition
    
    * salo locator + expected condition
    
    * :)
    
    * sleep aded
    
    * more sleep
    
    * EC check
    
    * no timeout
    
    * weird stuff
    
    * order with my locator
    
    * :)
    
    * fixed stuff
    
    * run all tests
    
    * PU@"#(/&#$)/($/
    
    * fixing long string in Locators.spec.ts
    
    * long string to small string separated by +

---
+++ src/page/Iframe.page.ts
@@ -0,0 +1,38 @@
+import { ElementFinder, $, promise, browser, ExpectedConditions } from 'protractor';
+
+export class IFramePage {
+  private get pageName (): ElementFinder {
+    return $('.page-title h1');
+  }
+
+  private get iFrame (): ElementFinder {
+    return $('#IF1');
+  }
+
+  public async getIFrameTitle (): Promise<string> {
+    await browser.wait(ExpectedConditions.presenceOf(this.pageName));
+    return this.pageName.getText();
+  }
+
+  public get getIFrameHeight(): promise.Promise<string> {
+    return this.iFrame.getCssValue('height');
+  }
+
+  public async changeIFrameHeight (newHeight: string): Promise<void> {
+    await browser.executeScript(`document.getElementById('IF1').style.height = '${newHeight}';`);
+  }
+
+  public async getPageName (): Promise<string> {
+    await browser.wait(ExpectedConditions.presenceOf(this.pageName));
+    return this.pageName.getText();
+  }
+
+  public async goToIFrameOne (): Promise<void> {
+    await browser.wait(ExpectedConditions.presenceOf(this.iFrame));
+    return browser.switchTo().frame(this.iFrame.getWebElement());
+  }
+
+  public returnToGlobalpage (): promise.Promise<void> {
+    return browser.switchTo().defaultContent();
+  }
+}
---
+++ test/IFrame.spec.ts
@@ -0,0 +1,70 @@
+import {
+  browser
+} from 'protractor';
+
+import {
+  IFramePage
+} from '../src/page/Iframe.page';
+
+describe('open browser', () => {
+
+  beforeAll(async () => {
+    await browser.get(
+      'http://toolsqa.com/iframe-practice-page/'
+    );
+  });
+
+  describe('checking global title', () => {
+    const iFramePage: IFramePage = new IFramePage();
+    let pageTitle: string;
+
+    beforeAll(async () => {
+      pageTitle = await iFramePage.getPageName();
+    });
+
+    it('checking global title', () => {
+      expect(pageTitle)
+      .toBe('IFrame practice page');
+    });
+
+    describe('change iframe height', () => {
+      const newHeight = '100px';
+      let actualIFrameHeight;
+
+      beforeAll(async () => {
+        await iFramePage.changeIFrameHeight(newHeight);
+        actualIFrameHeight = await iFramePage.getIFrameHeight;
+      });
+
+      it(`checking height to ${newHeight}`, () => {
+        expect(actualIFrameHeight).toBe(newHeight);
+      });
+
+      describe('checking iframe title', () => {
+        let iFrameTitle: string;
+
+        beforeAll(async () => {
+          await iFramePage.goToIFrameOne();
+          iFrameTitle = await iFramePage.getIFrameTitle();
+        });
+
+        it('Iframe name Check', () => {
+          expect(iFrameTitle)
+            .toBe('Automation Practice Form');
+        });
+
+        describe('return to globla and check name', () => {
+          beforeAll(async () => {
+            await iFramePage.returnToGlobalpage();
+            pageTitle = await iFramePage.getPageName();
+          });
+
+          it('return to global page check', () => {
+            expect(pageTitle)
+              .toBe('IFrame practice page');
+          });
+        });
+      });
+    });
+  });
+});
