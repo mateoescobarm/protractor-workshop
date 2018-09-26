e657e9ec9942545ef519bd0f6221554c9477a8a3
mateoescobarm <matesoescobarm@gmail.com>
2018-09-26 00:18:36 -0500

    headless

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,15 +4,21 @@ import { reporter } from './helpers/reporter';
 const firefoxConfig = {
   browserName: 'firefox',
   name: 'firefox-test',
-  shardTestFiles: true,
+  shardTestFiles: false,
   maxInstances: 1,
+  firefoxOptions: {
+    args: ['--headless', '--disable-gpu', '--window-size=800,600']
+  }
 };
 
 const chromeConfig = {
   browserName: 'chrome',
   name: 'chrome-test',
-  shardTestFiles: true,
+  shardTestFiles: false,
   maxInstances: 1,
+  chromeOptions: {
+    args: ['--headless', '--disable-gpu', '--window-size=800,600']
+  }
 };
 
 const multiCapabilities = [chromeConfig, firefoxConfig];
@@ -31,5 +37,11 @@ export const config: Config = {
     browser.manage().timeouts().implicitlyWait(0);
     browser.ignoreSynchronization = true;
     reporter();
-  }
+  },
+  // capabilities: {
+  //   browserName: 'chrome',
+  //   chromeOptions: {
+  //     args: ['--headless', '--disable-gpu', '--window-size=800,600']
+  //   }
+  // }
 };
